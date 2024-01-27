b7b7b7e3ed8b77a965fad0e61d8561b9111d6e9b
Arghonaut <lo.rivemale@gmail.com>
2024-01-27 12:20:59 +0100

    Fixed the needle animation. Fixed the needle rotation space. Remade the UI using prefabs.

--- Assets/Scripts/UI/PlayerUIBehaviour.cs
+++ Assets/Scripts/UI/PlayerUIBehaviour.cs
@@ -6,15 +6,22 @@ namespace UI
     public class PlayerUIBehaviour : MonoBehaviour
     {
         [SerializeField] private RectTransform _gaugeNeedle;
+        [SerializeField] private GameObject _cork;
         [SerializeField] private float _rotationSpeed;
         private readonly Vector2 _gaugeEmptyToFullRotation = new Vector2(-90f, 0f);
 
         private float currentPercentage = 0f;
 
+        // an example
         [ContextMenu("Add10Percent")]
         private void Add10Percent()
         {
             UpdateNeedleRotation(currentPercentage + 0.1f);
+        }
+
+        public void OnUseCork()
+        {
+            _cork.SetActive(false);
         }
 
         public void UpdateNeedleRotation(float gaugePercentage)
@@ -26,11 +33,12 @@ namespace UI
         private IEnumerator UpdateNeedle(float gaugePercentage) // percentage between 0 and 1f
         {
             float targetRotation = Mathf.Lerp(_gaugeEmptyToFullRotation.x, _gaugeEmptyToFullRotation.y, gaugePercentage);
+            float currentRotation = Mathf.Lerp(_gaugeEmptyToFullRotation.x, _gaugeEmptyToFullRotation.y, currentPercentage);
             float ellapsedTime = 0f;
 
             while (ellapsedTime < _rotationSpeed)
             {
-                _gaugeNeedle.rotation = Quaternion.Euler(_gaugeNeedle.eulerAngles.x, _gaugeNeedle.eulerAngles.y, Mathf.Lerp(_gaugeNeedle.eulerAngles.z, targetRotation, ellapsedTime / _rotationSpeed));
+                _gaugeNeedle.localRotation = Quaternion.Euler(_gaugeNeedle.eulerAngles.x, _gaugeNeedle.eulerAngles.y, Mathf.Lerp(currentRotation, targetRotation, ellapsedTime / _rotationSpeed));
                 yield return new WaitForEndOfFrame();
                 ellapsedTime += Time.deltaTime;
             }
