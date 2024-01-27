ee4878a13e4239a4a6cef710790c7beba5a6a5a8
Chefty <f.cheftel@gmail.com>
2024-01-27 15:27:27 +0100

    Working controls movement for multiple players relative to camera position

--- Assets/Scripts/Character/Properties/CharacterPropertiesFactory.cs
+++ Assets/Scripts/Character/Properties/CharacterPropertiesFactory.cs
@@ -3,7 +3,7 @@ namespace Character.Properties
     public static class CharacterPropertiesFactory
     {
         private const float InitialFartAmount = 100;
-        private const float CharacterSpeed = 1f;
+        private const float CharacterSpeed = 0.1f;
         private const int InitialNumberOfCorks = 1;
         
         public static ICharacterProperties Get(bool isNpc)
