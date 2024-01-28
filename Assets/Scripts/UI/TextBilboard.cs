ce79507bf8db95219da9b3d2627375b188f6ba7a
Arghonaut <lo.rivemale@gmail.com>
2024-01-28 01:20:28 +0100

    Made player name text bilboard main camera.

---
+++ Assets/Scripts/UI/TextBilboard.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TextBilboard : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+        // Cache the reference to the main camera to improve performance
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        // Check if the main camera is set
+        if (mainCamera == null) return;
+
+        // Make the text face the camera
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
+    }
+}
