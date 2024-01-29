00ab5e14d8b37004eb7f96cf2dcc2ccafbc8cb0b
Chefty <f.cheftel@gmail.com>
2024-01-29 14:40:31 +0100

    More polishing

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class GameConstants
 {
     public const float MAX_SECONDS_FARTING = 10f;
-    public const float DEFAULT_FART_VALUE = 3f;
+    public const float DEFAULT_FART_VALUE = 5f;
     public const float FART_COOLDOWN = 1f;
     public const float CORK_TIME = 5f;
 
